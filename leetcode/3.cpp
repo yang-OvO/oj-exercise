45605b7e9a59c19c0e71d4a2d9549809724feeda
litingyang <li_tingyang@outlook.com>
2026-04-20 11:17:56 +0800

    添加leetcode 3.cpp的正确性说明

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+	// 每次处理完字符s[i]后，
+	// 队列中保存的恰好是以s[i]结尾的最长无重复子串。
         int res = 0;
         deque<char> q;
         for (auto c : s) {
