2d0698dfc1db916c748b46abb4d81707bd3787f1
litingyang <li_tingyang@outlook.com>
2026-06-24 13:12:04 +0800

    添加lowestCommonAncestor的注释

--- leetcode/236.cpp
+++ leetcode/236.cpp
@@ -51,12 +51,16 @@ public:
         return result;
     }
 
-    // 若p和q均在于root树上，则返回lca
+    // 若p和q均在root树上，则返回lca
     // 若p和q有一个在root树上，则不存在lca，但是返回在的那个
     // 若p和q均不在root树上，则返回nullptr
-    // （语义好复杂，捋不清楚QAQ）
+    // （非常tricky，语义复杂，捋了半天才捋清楚QAQ）
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
+        // Base case包含三种情况
+        // 空树，返回nullptr
+        // root为p或q，另一个在树上，则root为lca
+        // root为p或q，另一个不在树上，则不存在lca，但是返回在的那个（即root）
         if (!root || root == p || root == q) {
             return root;
         }
@@ -64,13 +68,13 @@ public:
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-        // 若p和q分居于root的两棵子树上，则root为lca
+        // 若p和q在root的子树上并且位于异侧，则root为lca
         if (left && right) {
             return root;
         }
 
         // 这里包含三种情况
-        // 若p和q位于root的同一子树上，则返回lca（非空的那个）
+        // 若p和q位于root的子树上并且在同侧，则返回lca（非空的那个）
         // 若p和q有一个在root的子树上，另一个不在，则返回在的那个（非空的那个）
         // 若p和q不在root的任一子树上，则left和right均为空，返回nullptr
         return left ? left : right;
